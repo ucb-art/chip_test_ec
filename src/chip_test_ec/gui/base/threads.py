090e904fc6e922535620d357f6308b2d63f27fb1
Eric Chang <pkerichang@gmail.com>
2017-12-05 02:50:03 -0800

    1. update type hinting to reference Controller class.

--- src/chip_test_ec/gui/base/threads.py
+++ src/chip_test_ec/gui/base/threads.py
@@ -2,7 +2,7 @@
 
 """This module defines various threads related classes to perform time-consuming tasks outside of main event loop."""
 
-from typing import TYPE_CHECKING, Callable
+from typing import TYPE_CHECKING, Callable, Dict, Any
 
 from PyQt5 import QtCore
 
@@ -14,7 +14,7 @@ class WorkerThread(QtCore.QThread):
 
     update = QtCore.pyqtSignal(str)
 
-    def __init__(self, fun: Callable[['WorkerThread', Controller, ...], None], ctrl, params):
+    def __init__(self, fun: Callable[['WorkerThread', Controller, ...], None], ctrl: Controller, params: Dict[str, Any]):
         super(WorkerThread, self).__init__()
         self.fun = fun
         self.ctrl = ctrl
