2a8f0f1834ce911b371bfb45a813213c44019e27
Eric Chang <pkerichang@gmail.com>
2018-01-19 01:00:28 -0800

    1. refactor EyePlotBase so if configuration dictionary has missing    entries, an error will occur during the constructor.

--- src/chip_test_ec/backend/serdes/eye.py
+++ src/chip_test_ec/backend/serdes/eye.py
@@ -20,7 +20,12 @@ class EyePlotBase(object, metaclass=abc.ABCMeta):
     def __init__(self, thread: WorkerThread, ctrl: Controller, config: Dict[str, Any]):
         self.thread = thread
         self.ctrl = ctrl
-        self.config = config
+
+        self.ber = config['ber']
+        self.max_err = config['max_err']
+        self.y_guess = config.get('y_guess', None)
+        self.tvec = list(range(*config['t_range']))
+        self.yvec = list(range(*config['y_range']))
 
     @abc.abstractmethod
     def set_delay(self, val: int):
@@ -31,51 +36,42 @@ class EyePlotBase(object, metaclass=abc.ABCMeta):
         pass
 
     @abc.abstractmethod
-    def read_error(self, ber: float, max_err: int) -> int:
+    def read_error(self) -> int:
         return 0
 
     def run(self):
-        ber = self.config['ber']
-        max_err = self.config['max_err']
-
-        tvec = list(range(*self.config['t_range']))
-        yvec = list(range(*self.config['y_range']))
-
-        num_y = len(yvec)
-        if 'y_guess' in self.config:
-            guess_idx = bisect.bisect_left(yvec, self.config['y_guess'])
-        else:
-            guess_idx = num_y // 2
+        num_y = len(self.yvec)
+        guess_idx = num_y // 2 if self.y_guess is None else bisect.bisect_left(self.yvec, self.y_guess)
 
         try:
-            for t_idx, tval in enumerate(tvec):
+            for t_idx, tval in enumerate(self.tvec):
                 if self.thread.stop:
                     raise StopException()
                 # first, measure BER at guessed offset
                 self.set_delay(tval)
-                self.set_offset(yvec[guess_idx])
-                err_cnt = self.read_error(ber, max_err)
+                self.set_offset(self.yvec[guess_idx])
+                err_cnt = self.read_error()
                 self.thread.send(dict(t_idx=t_idx, y_idx=guess_idx, err_cnt=err_cnt))
 
                 if err_cnt == 0:
                     # the guessed offset is in the eye
                     # use binary search to find bottom eye edge
-                    bot_edge_idx = self._bin_search_eye_edge(t_idx, yvec, 0, guess_idx, True)
+                    bot_edge_idx = self._bin_search_eye_edge(t_idx, 0, guess_idx, True)
                     # mark all region below eye as max errors.
                     for idx in range(0, bot_edge_idx - 1):
-                        self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=max_err))
+                        self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=self.max_err))
                     # use binary search to find top eye edge
-                    top_edge_idx = self._bin_search_eye_edge(t_idx, yvec, guess_idx + 1, num_y, False)
+                    top_edge_idx = self._bin_search_eye_edge(t_idx, guess_idx + 1, num_y, False)
                     # mark all region above eye as max errors.
                     for idx in range(top_edge_idx + 2, num_y):
-                        self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=max_err))
+                        self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=self.max_err))
                 else:
                     # the guessed offset is not in the eye.
                     # assume we are below the eye, linear search bottom edge
-                    bot_edge_idx = self._lin_search_eye_edge(t_idx, yvec, guess_idx + 1, num_y)
+                    bot_edge_idx = self._lin_search_eye_edge(t_idx, guess_idx + 1, num_y)
                     if bot_edge_idx < 0:
                         # we did not find bottom edge.  Try finding top edge
-                        top_edge_idx = self._lin_search_eye_edge(t_idx, yvec, guess_idx - 1, -1)
+                        top_edge_idx = self._lin_search_eye_edge(t_idx, guess_idx - 1, -1)
                         if top_edge_idx < 0:
                             # we did not find top edge either.  This means we swept all possibilities,
                             # so just continue.
@@ -84,22 +80,22 @@ class EyePlotBase(object, metaclass=abc.ABCMeta):
                             # we found top edge
                             # mark all region above eye as max errors.
                             for idx in range(top_edge_idx + 2, num_y):
-                                self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=max_err))
+                                self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=self.max_err))
                             # use binary search to find bottom eye edge
-                            bot_edge_idx = self._bin_search_eye_edge(t_idx, yvec, 0, top_edge_idx, True)
+                            bot_edge_idx = self._bin_search_eye_edge(t_idx, 0, top_edge_idx, True)
                             # mark all region below eye as max errors.
                             for idx in range(0, bot_edge_idx - 1):
-                                self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=max_err))
+                                self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=self.max_err))
                     else:
                         # we found bottom edge
                         # mark all region below eye as max errors.
                         for idx in range(0, bot_edge_idx - 1):
-                            self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=max_err))
+                            self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=self.max_err))
                         # use binary search to find top eye edge
-                        top_edge_idx = self._bin_search_eye_edge(t_idx, yvec, bot_edge_idx + 1, num_y, False)
+                        top_edge_idx = self._bin_search_eye_edge(t_idx, bot_edge_idx + 1, num_y, False)
                         # mark all region above eye as max errors.
                         for idx in range(top_edge_idx + 2, num_y):
-                            self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=max_err))
+                            self.thread.send(dict(t_idx=t_idx, y_idx=idx, err_cnt=self.max_err))
 
                 # mark all region in the eye as no errors.
                 for idx in range(bot_edge_idx + 1, top_edge_idx):
@@ -111,10 +107,7 @@ class EyePlotBase(object, metaclass=abc.ABCMeta):
         except StopException:
             pass
 
-    def _bin_search_eye_edge(self, t_idx, yvec, bot_idx, top_idx, eye_on_top):
-        ber = self.config['ber']
-        max_err = self.config['max_err']
-
+    def _bin_search_eye_edge(self, t_idx, bot_idx, top_idx, eye_on_top):
         bin_iter = BinaryIterator(bot_idx, top_idx)
         edge_idx = top_idx if eye_on_top else bot_idx - 1
         while bin_iter.has_next():
@@ -122,9 +115,9 @@ class EyePlotBase(object, metaclass=abc.ABCMeta):
                 raise StopException()
 
             cur_idx = bin_iter.get_next()
-            self.set_offset(yvec[cur_idx])
+            self.set_offset(self.yvec[cur_idx])
 
-            err_cnt = self.read_error(ber, max_err)
+            err_cnt = self.read_error()
             self.thread.send(dict(t_idx=t_idx, y_idx=cur_idx, err_cnt=err_cnt))
 
             if err_cnt == 0:
@@ -140,18 +133,15 @@ class EyePlotBase(object, metaclass=abc.ABCMeta):
 
         return edge_idx
 
-    def _lin_search_eye_edge(self, t_idx, yvec, start_idx, stop_idx):
-        ber = self.config['ber']
-        max_err = self.config['max_err']
-
+    def _lin_search_eye_edge(self, t_idx, start_idx, stop_idx):
         step = 1 if stop_idx >= start_idx else -1
         for cur_idx in range(start_idx, stop_idx, step):
             if self.thread.stop:
                 raise StopException()
 
-            self.set_offset(yvec[cur_idx])
+            self.set_offset(self.yvec[cur_idx])
 
-            err_cnt = self.read_error(ber, max_err)
+            err_cnt = self.read_error()
             self.thread.send(dict(t_idx=t_idx, y_idx=cur_idx, err_cnt=err_cnt))
 
             if err_cnt == 0:
